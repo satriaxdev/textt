37314a75c26e6688d6a81b761cfd34fefc898022
satriaxdev <satriadeveloperz@gmail.com>
2025-11-17 00:31:36 +0700

    Add components/icons/AkbarIcon.tsx

---
+++ components/icons/AkbarIcon.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+export const AkbarIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+    <path d="M12 9V5H8" />
+    <rect width="16" height="12" x="4" y="9" rx="2" />
+    <path d="M2 15h2" />
+    <path d="M20 15h2" />
+    <path d="M15 14v2" />
+    <path d="M9 14v2" />
+  </svg>
+);
