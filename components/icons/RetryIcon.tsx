36d19073e80feb7255c64c8e4bd900b3543db8d6
satriaxdev <satriadeveloperz@gmail.com>
2025-11-17 00:31:13 +0700

    Add components/icons/RetryIcon.tsx

---
+++ components/icons/RetryIcon.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+
+export const RetryIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+    <path d="M1 4v6h6" />
+    <path d="M23 20v-6h-6" />
+    <path d="M20.49 9A9 9 0 0 0 7.54 5.49l-6 6" />
+    <path d="M3.51 15a9 9 0 0 0 12.95 3.51l6-6" />
+  </svg>
+);
