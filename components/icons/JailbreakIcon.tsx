af94bda72a93c674f311f836b4d387084b6ad2da
satriaxdev <satriadeveloperz@gmail.com>
2025-11-17 00:31:34 +0700

    Add components/icons/JailbreakIcon.tsx

---
+++ components/icons/JailbreakIcon.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+export const JailbreakIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+    <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.72"/>
+    <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.72-1.72"/>
+    <line x1="8" y1="2" x2="8" y2="5" />
+    <line x1="2" y1="8" x2="5" y2="8" />
+    <line x1="16" y1="19" x2="16" y2="22" />
+    <line x1="19" y1="16" x2="22" y2="16" />
+  </svg>
+);
