f8e0ed2deb487645ff5c7a83c34779fd25aa3148
satriaxdev <satriadeveloperz@gmail.com>
2025-11-17 00:31:35 +0700

    Add components/icons/AssistantIcon.tsx

---
+++ components/icons/AssistantIcon.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+export const AssistantIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+    <path d="M9.5 2.5a2.5 2.5 0 0 1 5 0"/>
+    <path d="M12 6a1 1 0 0 1 1 1v2a1 1 0 0 1-2 0V7a1 1 0 0 1 1-1Z"/>
+    <path d="M12 11.5a1 1 0 0 1 1 1v2a1 1 0 0 1-2 0v-2a1 1 0 0 1 1-1Z"/>
+    <path d="M5.5 14.5a1 1 0 0 1 1 1v2a1 1 0 0 1-2 0v-2a1 1 0 0 1 1-1Z"/>
+    <path d="M17.5 14.5a1 1 0 0 1 1 1v2a1 1 0 0 1-2 0v-2a1 1 0 0 1 1-1Z"/>
+    <path d="M12 21a9 9 0 0 1-9-9 9 9 0 0 1 9-9 9 9 0 0 1 9 9 9 9 0 0 1-9 9Z"/>
+  </svg>
+);
